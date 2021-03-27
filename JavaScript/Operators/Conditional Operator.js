1e1bc6773aa2bc77db04f6b87d61411508a699c5
Exxon <tomfagan03@gmail.com>
2021-03-27 12:52:46 +0000

    Added readme and conditional operator

---
+++ JavaScript/Operators/Conditional Operator.js
@@ -0,0 +1,25 @@
+/**
+ * The conditional operator (Also known as the "ternary operator") is the only JavaScript operator that takes three operands.
+ * The operator can have one of two values based on the condition. The syntax is:
+ * 
+ * condition ? val1 : val2
+ * 
+ * If the condition is true then it returns val1 otherwise it will return val2. 
+ * 
+ * Examples:
+ */
+
+
+
+/**
+ * This will result in canDrink = 'Old enough to drink' if age is greater than or equal to 18, otherwise it result in canDrink = 'Too young to drink'
+ */
+
+const age = 19;
+
+const canDrink = (age >= 18) ? 'Old enough to drink' : 'Too young to drink';
+
+console.log(canDrink); // Expected output --> 'Old enough to drink'
+
+
+
