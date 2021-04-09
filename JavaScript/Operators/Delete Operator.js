63f03d908b4c2fea9713254032d2b472fc30448d
Exxon <tomfagan03@gmail.com>
2021-04-09 23:58:25 +0100

    Added delete operator

---
+++ JavaScript/Operators/Delete Operator.js
@@ -0,0 +1,32 @@
+/**
+ * The JavaScript 'delete' operator removes a property from
+ * an object. If no more references to the same property are held,
+ * it is eventually released automatically.
+ * 
+ * Documentation - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
+ * 
+ * In the example below, we have a variable called person which
+ * is an object. The object includes the name and age, we want to 
+ * delete the age property using the 'delete' operator.
+ */
+
+const person = {
+    name: 'Exxon',
+    age: 18
+};
+
+console.log(person);
+/** This will log the following:
+ * {
+ *   age: 18,
+ *   name: 'Exxon'
+ * }
+ */
+
+delete person.name;
+console.log(person);
+/** This will log the following:
+ * {
+ *   age: 18,
+ * }
+ */
