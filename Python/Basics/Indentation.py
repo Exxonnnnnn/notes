dcee5c4e236ab4f854dc5cde1b436265cdd2e744
Exxon <tomfagan03@gmail.com>
2021-05-14 01:19:56 +0100

    Finished Python Basics (I think?)

---
+++ Python/Basics/Indentation.py
@@ -0,0 +1,17 @@
+"""
+Python Indentation refers to the spaces at the beginning of a code line. 
+In Python, indentation is used to indicate a block of code. For example, 
+if we were to do an if statement then we would need to use indentation 
+to show what to do after the if statement.
+
+Documentation - https://www.w3schools.com/python/python_syntax.asp
+
+In the example below we will have a simple if statement. If we were to skip the
+indentation then we would get a Syntax Error.
+"""
+
+if 5 > 2:
+    print("Five is greater than two!") # We have used indentation to show what it should do if the if statement is true
+
+
+
--- Python/Basics/Variables.py
+++ Python/Basics/Variables.py
@@ -5,7 +5,9 @@ also change the type and the value of a variable after it has been set.
 
 When creating a new variable there is certain names that you cannot have. You
 cannot have a space or (-) in the variable name. The name also cannot start with
-a number, it must start with a letter or an underscore (_).
+a number, it must start with a letter or an underscore (_). Variable names are
+case-sensitive meaning that you can have a variable called "name" and another called
+"Name".
 
 Documentation - https://www.w3schools.com/python/python_variables.asp
 
@@ -26,3 +28,50 @@ print(myName) # This will log - Tom
 print(myAge) # This will log - 19
 
 # Notice how you do not get any errors by reassigning the value or the type of the variable.
+
+
+"""
+We can also asign values to multiple variables in one line. This can be done by listing the
+variable names seperated by a comma (,) and then an equals sign and the values seperated by
+a comma (,).
+
+Documentation - https://www.w3schools.com/python/python_variables_multiple.asp
+
+In the example below we will create three new variables, "hello", "foo" and "bar".
+"""
+
+hello, foo, bar = "Hello World!", "foo", "bar"
+
+print(hello) # This will log - Hello World!
+print(foo) # This will log - foo
+print(bar) # This will log - bar
+
+
+"""
+We can also assin the same value to multiple variables in one line. This is done by listing 
+each variable name seperated by " = " followed by an equals sign and then the value of the 
+variables.
+
+In the example below we will create two new variables, "exxon" and "Exxon".
+"""
+
+exxon = Exxon = "My name is Exxon!"
+
+print(exxon) # This will log - My name is Exxon!
+print(Exxon) # This will log - My name is Exxon!
+
+
+"""
+The final thing we will look at is unpacking a collection. If we have a collection of values in
+a list we can extract the values into variables. This is called "unpacking".
+
+In the example below we will have an array of fruits that we will unpack to three variables, "x",
+"y", and "z".
+"""
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x) # This will log - apple
+print(y) # This will log - banana
+print(z) # This will log - cheery
